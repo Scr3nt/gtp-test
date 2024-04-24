b195ea979de6c10f32c594bc74cc3fbd74371563
Gino Dzin <56580186+Scr3nt@users.noreply.github.com>
2024-04-24 09:47:01 +0200

    :recycle: Refacto EmployeeList

--- src/app/(mainstack)/(tabs)/(employee)/employee.tsx
+++ src/app/(mainstack)/(tabs)/(employee)/employee.tsx
@@ -1,30 +1,18 @@
 import { router } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { FlatList, StyleSheet, TouchableOpacity, View } from "react-native";
-import { useQuery } from "react-query";
+import { StyleSheet } from "react-native";
 
-import { getAllEmployees } from "@/src/api/APIEmployee";
-import Avatar from "@/src/components/Avatar/Avatar";
 import Button from "@/src/components/Button/Button";
-import Empty from "@/src/components/Empty/Empty";
 import Page from "@/src/components/Page/Page";
-import Separator from "@/src/components/Separator/Separator";
 import Text from "@/src/components/Text/Text";
 import { useTheme } from "@/src/context/theme";
-import EmployeeListLoading from "@/src/features/App/Employee/components/EmployeeList/EmployeeListLoading";
-import { queryKeys } from "@/src/queryKeys";
+import EmployeeList from "@/src/features/App/Employee/components/EmployeeList/EmployeeList";
 import { Theme } from "@/src/theme/theme";
-import { formatTimestampzToDate } from "@/src/utils/formatDate";
 import { Ionicons } from "@expo/vector-icons";
 
 export default function EmployeePage() {
   const theme = useTheme();
   const styles = employeeStyles(theme);
-
-  const employeeList = useQuery({
-    queryKey: [queryKeys.employee.getAllEmployees],
-    queryFn: () => getAllEmployees(),
-  });
 
   return (
     <Page style={styles.container}>
@@ -33,58 +21,7 @@ export default function EmployeePage() {
         <Text type="semi-bold" preset="title03" style={styles.title}>
           Liste des employés
         </Text>
-        <FlatList
-          scrollEnabled={false}
-          data={employeeList.data}
-          renderItem={({ item }) => (
-            <TouchableOpacity
-              style={{
-                paddingVertical: theme.spacing.s,
-                paddingHorizontal: theme.spacing.m,
-              }}
-              activeOpacity={0.8}
-              onPress={() =>
-                router.navigate({
-                  pathname: "/edit_employee",
-                  params: { id: item.id },
-                })
-              }
-            >
-              <View style={styles.employee_container}>
-                <Avatar size={40} initials={item.name.slice(0, 1)} />
-                <View>
-                  <Text type="semi-bold">{item.name}</Text>
-                  <Text preset="caption">
-                    Crée le {formatTimestampzToDate(item.created_at)}
-                  </Text>
-                </View>
-              </View>
-            </TouchableOpacity>
-          )}
-          keyExtractor={(item) => item.id.toString()}
-          // eslint-disable-next-line react/no-unstable-nested-components
-          ItemSeparatorComponent={() => <Separator />}
-          // eslint-disable-next-line react/no-unstable-nested-components
-          ListFooterComponent={() => {
-            if (employeeList.isFetching) {
-              return <EmployeeListLoading />;
-            }
-          }}
-          // eslint-disable-next-line react/no-unstable-nested-components
-          ListEmptyComponent={() => {
-            if (employeeList.isFetching) {
-              return;
-            }
-            return (
-              <Empty style={{ padding: theme.spacing.l }}>
-                <Text preset="title03" type="bold" style={styles.space_bottom}>
-                  Aucune tâche
-                </Text>
-                <Text style={styles.space_bottom}>Veulliez en créer une</Text>
-              </Empty>
-            );
-          }}
-        />
+        <EmployeeList />
       </Page>
       <Button
         onPress={() => {
@@ -129,14 +66,6 @@ const employeeStyles = (theme: Theme) => {
     },
     create_employee_text: {
       color: theme.colors.blue11,
-    },
-    employee_container: {
-      flexDirection: "row",
-      alignItems: "center",
-      gap: theme.spacing.s,
-    },
-    space_bottom: {
-      marginBottom: theme.spacing.s,
     },
   });
 
---
+++ src/features/App/Employee/components/EmployeeList/EmployeeList.tsx
@@ -0,0 +1,95 @@
+import { router } from "expo-router";
+import { FlatList, StyleSheet, TouchableOpacity, View } from "react-native";
+import { useQuery } from "react-query";
+
+import { getAllEmployees } from "@/src/api/APIEmployee";
+import Avatar from "@/src/components/Avatar/Avatar";
+import Empty from "@/src/components/Empty/Empty";
+import Separator from "@/src/components/Separator/Separator";
+import Text from "@/src/components/Text/Text";
+import { useTheme } from "@/src/context/theme";
+import { queryKeys } from "@/src/queryKeys";
+import { Theme } from "@/src/theme/theme";
+import { formatTimestampzToDate } from "@/src/utils/formatDate";
+
+import EmployeeListLoading from "./EmployeeListLoading";
+
+export default function EmployeeList() {
+  const theme = useTheme();
+  const styles = employeeListStyles(theme);
+
+  const employeeList = useQuery({
+    queryKey: [queryKeys.employee.getAllEmployees],
+    queryFn: () => getAllEmployees(),
+  });
+
+  return (
+    <FlatList
+      scrollEnabled={false}
+      data={employeeList.data}
+      renderItem={({ item }) => (
+        <TouchableOpacity
+          style={{
+            paddingVertical: theme.spacing.s,
+            paddingHorizontal: theme.spacing.m,
+          }}
+          activeOpacity={0.8}
+          onPress={() =>
+            router.navigate({
+              pathname: "/edit_employee",
+              params: { id: item.id },
+            })
+          }
+        >
+          <View style={styles.employee_container}>
+            <Avatar size={40} initials={item.name.slice(0, 1)} />
+            <View>
+              <Text type="semi-bold">{item.name}</Text>
+              <Text preset="caption">
+                Crée le {formatTimestampzToDate(item.created_at)}
+              </Text>
+            </View>
+          </View>
+        </TouchableOpacity>
+      )}
+      keyExtractor={(item) => item.id.toString()}
+      // eslint-disable-next-line react/no-unstable-nested-components
+      ItemSeparatorComponent={() => <Separator />}
+      // eslint-disable-next-line react/no-unstable-nested-components
+      ListFooterComponent={() => {
+        if (employeeList.isFetching) {
+          return <EmployeeListLoading />;
+        }
+      }}
+      // eslint-disable-next-line react/no-unstable-nested-components
+      ListEmptyComponent={() => {
+        if (employeeList.isFetching) {
+          return;
+        }
+        return (
+          <Empty style={{ padding: theme.spacing.l }}>
+            <Text preset="title03" type="bold" style={styles.space_bottom}>
+              Aucun employé
+            </Text>
+            <Text style={styles.space_bottom}>Veulliez en créer un</Text>
+          </Empty>
+        );
+      }}
+    />
+  );
+}
+
+const employeeListStyles = (theme: Theme) => {
+  const styles = StyleSheet.create({
+    employee_container: {
+      flexDirection: "row",
+      alignItems: "center",
+      gap: theme.spacing.s,
+    },
+    space_bottom: {
+      marginBottom: theme.spacing.s,
+    },
+  });
+
+  return styles;
+};
